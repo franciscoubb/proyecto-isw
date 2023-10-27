9ffb574c22c2db309a83cdae39e71ba61cac4c9f
Daniel Gallegos <daniel.gallegos1901@alumnos.ubiobio.cl>
2023-10-27 00:59:15 -0300

    Requerimientos funcionales Daniel Gallegos

--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -21,3 +21,52 @@ router.use("/auth", authRoutes);
 
 // Exporta el enrutador
 module.exports = router;
+
+// Importa los módulos necesarios
+const { DebtService, DebtModel } = require("./services/debt.service");
+const { sendMail } = require("./utils/sendMail");
+
+// Define la ruta para realizar un pago
+router.post("/pagos", authenticationMiddleware, async (req, res) => {
+  // Valida el payload
+    const { monto, idDeuda } = req.body;
+if (!monto || !idDeuda) {
+    return res.status(400).json({
+    error: "El payload es inválido.",
+    });
+}
+
+  // Realiza el pago
+const { deuda } = await DebtService.pay({
+    monto,
+    idDeuda,
+});
+
+  // Actualiza el estado de la deuda
+await DebtModel.update({
+    id,
+    status: deuda.status,
+});
+
+  // Envía el correo electrónico
+await sendMail({
+    to: req.user.email,
+    subject: "Pago realizado",
+    text: "El pago de la deuda con ID ${deuda.id} ha sido realizado exitosamente.",
+});
+
+  // Devuelve el estado de la deuda
+return res.json(deuda);
+});
+
+// Define la ruta para ver las deudas del usuario
+router.get("/deudas", authenticationMiddleware, async (req, res) => {
+  // Obtiene el usuario autenticado
+const user = await UserService.findById(req.user.id);
+
+  // Devuelve la lista de deudas
+return res.json(user.deudas);
+});
+
+// Exporta el enrutador
+module.exports = router;
